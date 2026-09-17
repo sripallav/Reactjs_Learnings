fb639ee2e4d88c98d22e94159d6addf1df264053
Sri Pallav <sripallavyerram@gmail.com>
2026-09-17 21:16:44 +0530

    Person.jsx

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,13 +1,38 @@
-const name = "Pallav"
-const age = 22
-function Person(props){
-    
-    return(
+const name = "Pallav";
+const age = 22;
+const car = "BMW";
+const colour = "red";
+
+function Person(props) {
+
+    var p = {
+        color: "yellow",
+        border: "5px solid cyan"
+    };
+
+    return (
         <>
-        <h1>Hello my name is {props.name}</h1>
-        <p>I am {props.age} years old.</p>
+            {/* inline css */}
+            <h1 style={{ color: "blue", backgroundColor: "yellow" }}>
+                Hello my name is {props.name}
+            </h1>
+
+            {/* internal css */}
+            <p style={p}>
+                My Favorite car is {props.car} and colour is {props.colour}
+            </p>
+
+            <p style={{
+                backgroundColor: props.bgc,
+                color: props.clr
+            }}>
+                I am Learning React
+            </p>
+
+            {/* ternary operator */}
+            {age >= 18 ? "Adult" : "Minor"}
         </>
-    )
+    );
 }
 
-export default Person
+export default Person;
