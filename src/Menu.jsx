d3196aa8c8422800d96f91f7a4c9f6ccc0ef91db
Sri Pallav <sripallavyerram@gmail.com>
2026-09-17 21:18:08 +0530

    Menu.jsx

---
+++ src/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Menu extends React.Component
+{
+    render()
+    {
+        return(
+            <>
+            <h1>This is class component</h1>
+            <p>we cannot extend more than one class</p>
+            </>
+        )
+    }
+}
+
+export default Menu
