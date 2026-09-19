ac8c0274c0d94de3dccc40dd3472edc3340adb01
Sri Pallav <sripallavyerram@gmail.com>
2026-09-19 21:18:12 +0530

    src/Product1.jsx

---
+++ src/Product1.jsx
@@ -0,0 +1,18 @@
+function Product1 (props){
+    return (
+
+        <>
+        <h1>Product_Name : {props.name}</h1>
+        <p>Product_Price : {props.price}</p>
+        <p>Product_Brand : {props.Brand}</p>
+        
+        
+        </>
+
+
+
+    );
+
+}
+
+export default Product1
