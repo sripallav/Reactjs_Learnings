62df204b54c128cc93b1a333f9206d3c1baab14f
Sri Pallav <sripallavyerram@gmail.com>
2026-09-19 21:19:23 +0530

    src/Student.jsx

---
+++ src/Student.jsx
@@ -0,0 +1,18 @@
+function Student(props){
+    return (
+        <>
+
+        <h2>Student Name : {props.name}</h2>
+        <p>age: {props.age}</p>
+        <p>course: {props.course}</p>
+        <p>city: {props.city}</p>
+
+
+        </>
+
+
+
+    );
+}
+
+export default Student
