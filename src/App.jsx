86df4b1d58112d6555276359617240a42cca8288
Sri Pallav <sripallavyerram@gmail.com>
2026-09-19 21:20:25 +0530

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import Student from './Student'
+import Product from './Product'
+import Password from './Password'
 
-function App(){
+function App() {
 
-  return (
-    <h1> good afternoon</h1>
-  )
+    return (
+        <>
+            <Student
+                name="Sripallav"
+                age={22}
+                course="Java Full Stack"
+                city="Hyderabad"
+            />
 
+            <Product
+                name="Laptop"
+                price={50000}
+                brand="Dell"
+            />
+
+            <Product
+                name="Mobile"
+                price={60000}
+                brand="iPhone"
+            />
+
+            <Product
+                name="Tab"
+                price={50000}
+                brand="Lenovo"
+            />
+
+            <Password />
+        </>
+    );
 }
 
 export default App
