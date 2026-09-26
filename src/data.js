b4d7e6ed8a543bd9a11dac479963ee64bdd8c073
Sri Pallav <sripallavyerram@gmail.com>
2026-09-26 20:38:47 +0530

    src/data.js

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const students = [
+    {
+        id: 1,
+        name: "Ravi",
+        marks: 85
+    },
+    {
+        id: 2,
+        name: "Likhitha",
+        marks: 72
+    },
+    {
+        id: 3,
+        name: "Praveena",
+        marks: 50
+    },
+    {
+        id: 4,
+        name: "Akash",
+        marks: 35
+    }
+];
+
+export default students;
